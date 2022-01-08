1d0efcdb733f026095366adbd2f01da6441e11fc
Sravan Kumar K <35738653+SravanGameDev@users.noreply.github.com>
2022-01-08 22:03:03 +0000

    Chest box big fixed and audio added

--- Assets/Scripts/OpenChest.cs
+++ Assets/Scripts/OpenChest.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class OpenChest : MonoBehaviour
 {
     private Animator animator;
+    private AudioSource audioBonus;
+    public AudioClip audioClip;
 
     private void Awake()
     {
         animator = this.GetComponent<Animator>();
+        audioBonus = this.GetComponent<AudioSource>();
     }
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         animator.SetBool("Open", true);
+        audioBonus.PlayOneShot(audioClip);
     }
 
     private void OnTriggerExit2D(Collider2D collision)
